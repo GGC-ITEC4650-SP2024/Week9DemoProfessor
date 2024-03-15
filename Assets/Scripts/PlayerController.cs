9366d76dd1fb78ca30d1be773e4395215b601850
Sebastien Siva <ssiva@ggc.edu>
2024-03-15 08:45:22 -0400

    Lecture_3_14_23_PreQuiz

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,11 +6,16 @@ public class PlayerController : MonoBehaviour
 {
     Rigidbody2D myBod;
     public float speed;
+    public float jumpPower;
+    public int maxJumps;
+
+    int numJumps;
 
     // Start is called before the first frame update
     void Start()
     {
-        myBod = GetComponent<Rigidbody2D>();    
+        myBod = GetComponent<Rigidbody2D>();
+        numJumps = 0;
     }
 
     // Update is called once per frame
@@ -19,9 +24,18 @@ public class PlayerController : MonoBehaviour
         float h = Input.GetAxis("Horizontal");
         Vector2 v = myBod.velocity;
         v.x = h * speed;
-
-
+        if(Input.GetButtonDown("Jump") && numJumps > 0) {
+            v.y = jumpPower;
+            numJumps--;
+        }
         myBod.velocity = v;
-        
+
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.tag == "Ground") {
+            numJumps = maxJumps;
+        }
     }
 }
